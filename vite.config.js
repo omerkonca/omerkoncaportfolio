c952afd599ee2e115f27a720caef83e64b27a603
Ömer Faruk KONCA <190541018@firat.edu.tr>
2024-07-14 22:34:08 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ['C:/Users/omerk/Desktop/proje/codecretportfolio-main/public/logo-black.png']
-    }
-  }
+    assetsInclude: ['logo-black.png'],
+  },
 })
