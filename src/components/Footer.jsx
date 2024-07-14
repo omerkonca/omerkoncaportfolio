e2bd39e5f19353e49b75db9eaa73a7a0ffbb1646
Ömer Faruk KONCA <190541018@firat.edu.tr>
2024-07-14 22:26:05 +0300

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,13 +14,13 @@ const Footer = () => {
     <footer className="mt-8 sm:px-[140px] sm:py-10 px-6 py-4 bg-black mr-3 w-full ">
       <div className="flex justify-between">
         <div>
-          <p className="text-sm">Copyright © 2023. All rights are reserved</p>
+          <p className="text-sm">Copyright © 2024. All rights are reserved</p>
           <div className="flex mt-2 gap-3">
             <AiFillLinkedin
               className="icon-social  cursor-pointer"
               onClick={() =>
                 window.open(
-                  "https://www.linkedin.com/in/mohammed-malek-alkhodary-47a763243/",
+                  "https://www.linkedin.com/in/omerkonca/",
                   "_blank"
                 )
               }
@@ -28,19 +28,19 @@ const Footer = () => {
             <AiFillInstagram
               className="icon-social cursor-pointer"
               onClick={() =>
-                window.open("https://www.instagram.com/codecret/", "_blank")
+                window.open("https://www.instagram.com/omerkoncaa/", "_blank")
               }
             />
             <AiFillGithub
               className="icon-social cursor-pointer"
               onClick={() =>
-                window.open("https://github.com/codecret", "_blank")
+                window.open("https://github.com/omerkonca", "_blank")
               }
             />
             <AiFillTwitterCircle
               className="icon-social cursor-pointer"
               onClick={() =>
-                window.open("https://twitter.com/codecret", "_blank")
+                window.open("https://twitter.com/omrkonca", "_blank")
               }
             />
           </div>
