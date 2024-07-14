3cc860d416e6e87649dff7caaef2bfc24dc03365
Ömer Faruk KONCA <190541018@firat.edu.tr>
2024-07-14 22:30:05 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
-import "./index.css";
+import "C:/Users/omerk/Desktop/proje/codecretportfolio-main/src/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
